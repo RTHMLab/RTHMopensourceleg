6ed7040ed37bcce3caa80b73d8fc78f0dba72203
gracecordle <gec20c@fsu.edu>
2024-06-05 18:13:05 +0100

    Basic motion for just the ankle

---
+++ examples/basic_motion_ankle.py
@@ -0,0 +1,47 @@
+"""
+A basic motion script that moves the osl knee through their range of motion.
+This script can be helpful when getting started to make sure the OSL is functional.
+
+Kevin Best
+Neurobionics Lab
+Robotics Department
+University of Michigan
+October 26, 2023
+"""
+
+import numpy as np
+
+from opensourceleg.osl import OpenSourceLeg
+from opensourceleg.tools import units
+
+osl = OpenSourceLeg(frequency=200)
+osl.add_joint("ankle", gear_ratio=9 * 83 / 18)
+
+
+def make_periodic_traj_func(period, minimum, maximum):
+    amplitude = (maximum - minimum) / 2
+    mean = amplitude + minimum
+    return lambda t: amplitude * np.cos(t * 2 * np.pi / period) + mean
+
+
+ankle_traj = make_periodic_traj_func(10, -20, 20)
+
+with osl:
+    osl.home()
+    input("Homing complete: Press enter to continue")
+    osl.ankle.set_mode(osl.ankle.control_modes.position)
+    osl.ankle.set_position_gains(kp=5)
+
+    for t in osl.clock:
+        osl.update()
+        ankle_setpoint = units.convert_to_default(ankle_traj(t), units.position.deg)
+        osl.ankle.set_output_position(ankle_setpoint)
+        print(
+            "Ankle Desired {:+.2f} rad, Ankle Actual {:+.2f} rad".format(
+                ankle_setpoint,
+                osl.ankle.output_position,
+            ),
+            end="\r",
+        )
+
+print("\n")
