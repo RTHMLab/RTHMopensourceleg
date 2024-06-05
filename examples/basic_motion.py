92c4fd4cc13dba8342a37afa4440b47e93097c58
gracecordle <168229323+gracecordle@users.noreply.github.com>
2024-06-05 12:35:39 -0400

    Update basic_motion.py

--- examples/basic_motion.py
+++ examples/basic_motion.py
@@ -2,8 +2,6 @@
 A basic motion script that moves the osl joints through their range of motion.
 This script can be helpful when getting started to make sure the OSL is functional.
 
-Testing 1 2 
-
 Kevin Best
 Neurobionics Lab
 Robotics Department
